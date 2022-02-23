51603efce8ad9d62e064e183aeea36f6396002a8
Sergey Emirzakov <im.seem16@gmail.com>
2022-02-23 18:48:47 +0300

    Reactoring

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://www.yourdomain.tld',
+    siteUrl: 'http://sergeyemirzakov.gatsbyjs.io/',
     title: '_sergeyemirzakov',
   },
   plugins: [
